78485bd5f21fb92476f0f04668a125e43b497e81
feldaega17 <156696112+feldaega17@users.noreply.github.com>
2024-12-03 18:55:56 +0700

    feat: backend authentication 2

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,8 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import authAPI from "./routes/auth.routes.js";
+import messageAPI from "./routes/message.routes.js";
 import connectMongoDB from "./database/connectMongoDB.js";
 
 const app = express();
@@ -9,8 +11,10 @@ const PORT = process.env.PORT || 3000;
 dotenv.config();
 
 app.use(express.json()); // parse incoming req with JSON payloads
+app.use(cookieParser());
 
 app.use("/api/auth", authAPI);
+app.use("/api/messages", messageAPI);
 
 // app.get("/", (req, res) => {
 //   res.send("Hello!");

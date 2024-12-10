8cd20b798f26057e54d47e264fcc1e919678288f
feldaega17 <156696112+feldaega17@users.noreply.github.com>
2024-12-10 21:03:23 +0700

    add all

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from "path";
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -9,14 +10,22 @@ import { app, server } from "./socket/socket.js";
 
 const PORT = process.env.PORT ||  5000;
 
+const __dirname = path.resolve();
+
 dotenv.config();
 
-app.use(express.json()); // parse incoming req with JSON payloads
+app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authAPI);
 app.use("/api/messages", messageAPI);
 app.use("/api/users", userAPI)
+
+app.use(express.static(path.join(__dirname, "/frontend/dist"))) 
+
+app.get("*", (req,res) => {
+  res.sendFile(path.join(__dirname,"frontend", "dist", "index.html"))
+})
 
 // app.get("/", (req, res) => {
 //   res.send("Hello!");
